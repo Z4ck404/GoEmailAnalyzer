cb87db6d29ca898401524fd1465d2a3223a2f547
Zakaria EL BAZI <zakaria@elbazi.me>
2022-11-29 01:53:03 +0100

    add a basic cmd with kango package

--- main.go
+++ main.go
@@ -1,3 +1,18 @@
-package goemailanalyzer
+package main
 
-// let the Golang journy begin
+import (
+	cmd "emailanlyzer/m/commands"
+
+	"github.com/alecthomas/kong"
+)
+
+var CLI struct {
+	Scan cmd.ScanCLI `cmd:"" help:"Scan a eml file"`
+}
+
+func main() {
+	ctx := kong.Parse(&CLI)
+	switch ctx.Command() {
+	// cmds will be called here
+	}
+}
