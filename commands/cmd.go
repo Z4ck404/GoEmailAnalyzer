b189e749f702525372603a8d7f5f711a9798fa6d
Zakaria EL BAZI <zakaria@elbazi.me>
2022-12-02 01:03:55 +0100

    fix: add a basic build/test golang ci

--- commands/cmd.go
+++ commands/cmd.go
@@ -1,11 +1,11 @@
 package commands
 
 type ScanCLI struct {
+	Filename string `name:"filename" help:"The eml file name to scan" required:""`
+
 	Headers      bool `help:"Get the headers of the email"`
 	Hash         bool `help:"Get the hash of the email"`
 	Links        bool `help:"Get the links in the email file"`
 	Digests      bool `help:"Get the digests of the eml file"`
 	Attachements bool `help:"Get the attachments from the eml file"`
-
-	Filename string `name:"filename" help:"The eml file name to scan" required:""`
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module emailanlyzer/m
 go 1.19
 
 require github.com/alecthomas/kong v0.7.1
+
+require github.com/DusanKasan/parsemail v1.2.0 // indirect
