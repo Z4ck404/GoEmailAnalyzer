2f4bf4934c62eb77da949332ee85f83befed50c5
Zakaria EL BAZI <zakaria@elbazi.me>
2022-12-13 02:27:42 +0100

    fixup! adding formated output to all commands

--- commands/utils.go
+++ commands/utils.go
@@ -1,7 +1,6 @@
 package commands
 
 import (
-	"encoding/json"
 	"fmt"
 	"os"
 
@@ -11,13 +10,13 @@ import (
 )
 
 type attribute struct {
-	key   string
-	value string
+	Key   string
+	Value string
 }
 
 type contentObject struct {
-	name       string
-	attributes []attribute
+	Name       string
+	Attributes []eml.Header
 }
 
 func readEmlFile(filePath string) (eml.Message, error) {
@@ -35,44 +34,34 @@ func readEmlFile(filePath string) (eml.Message, error) {
 
 func getHeaders(emlContent eml.Message) error {
 
-	// show all the headers
+	//show all the headers
+
+	//ignore_fields := []string{"FullHeaders", "OptHeaders"}
+	headerContent := contentObject{
+		Name:       "header",
+		Attributes: emlContent.HeaderInfo.FullHeaders,
+	}
+
 	// v := reflect.ValueOf(emlContent.HeaderInfo)
 	// typeOfS := v.Type()
 
 	// for i := 0; i < v.NumField(); i++ {
-	// 	fmt.Printf("%s :\t %v\n", typeOfS.Field(i).Name, v.Field(i).Interface())
+	// 	headerFiledName := typeOfS.Field(i).Name
+	// 	headerFiledValue := v.Field(i).Interface()
+
+	// 	for _, element := range ignore_fields {
+	// 		if headerFiledName != element {
+	// 			fmt.Print(headerFiledName)
+	// 			headerField := attribute{
+	// 				key:   headerFiledName,
+	// 				value: fmt.Sprintf("%v", headerFiledValue),
+	// 			}
+	// 			headerContent.attributes = append(headerContent.attributes, headerField)
+	// 		}
+	// 	}
+
 	// }
-	headerInfo := emlContent.HeaderInfo
-	sentTo, _ := json.Marshal(headerInfo.To)
-	headerContent := contentObject{
-		name: "header",
-		attributes: []attribute{
-			{
-				key:   "date",
-				value: headerInfo.Date.String(),
-			},
-			{
-				key:   "sender",
-				value: headerInfo.Sender.String(),
-			},
-			{
-				key:   "subject",
-				value: headerInfo.Subject,
-			},
-			{
-				key:   "Sent to",
-				value: string(sentTo),
-			},
-			{
-				key:   "subject",
-				value: headerInfo.Subject,
-			},
-			{
-				key:   "subject",
-				value: headerInfo.Subject,
-			},
-		},
-	}
+
 	_prettyTable(headerContent)
 	return nil
 }
@@ -105,9 +94,9 @@ func getLinks() error {
 func _prettyTable(c contentObject) {
 	t := table.NewWriter()
 	t.SetOutputMirror(os.Stdout)
-	t.AppendHeader(table.Row{fmt.Sprintf("%s Field", c.name), fmt.Sprintf("%v Value", c.name)})
-	for _, attribute := range c.attributes {
-		t.AppendRow([]interface{}{attribute.key, attribute.value})
+	t.AppendHeader(table.Row{fmt.Sprintf("%s Field", c.Name), fmt.Sprintf("%v Value", c.Name)})
+	for _, attribute := range c.Attributes {
+		t.AppendRow([]interface{}{attribute.Key, attribute.Value})
 		t.AppendRow([]interface{}{" ", " "})
 	}
 	t.Render()
